09f8eee0625c63d0bd3f11b90e9291a8962de712
Alireza <alireza.gholami403@gmail.com>
2021-09-24 18:33:20 +0330

    Inital Commit

---
+++ app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+html {
+  background-color: rgba(2, 19, 9, 0.87);
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding-top: 3rem;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
